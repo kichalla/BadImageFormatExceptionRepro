584c3647ba5a0775a97c4ec826fc1cc440d63da3
Kiran Challa <kichalla@microsoft.com>
2017-03-23 10:56:33 -0700

    updated

--- BadImageFormatExceptionRepro/Program.cs
+++ BadImageFormatExceptionRepro/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using Moq;
 
 namespace BadImageFormatExceptionRepro
 {
@@ -6,7 +8,29 @@ namespace BadImageFormatExceptionRepro
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Press any key to invoke the method causing the exception");
+            Console.ReadKey();
+
+            // In a separate method so that we can see that the exception happens during JIT compilation
+            Test();
+
+            Console.WriteLine("Done");
         }
+
+        static void Test()
+        {
+            var headersHandler = new Mock<IHttpHeadersHandler>();
+            headersHandler
+                .Setup(handler => handler.OnHeader(It.IsAny<Span<byte>>(), It.IsAny<Span<byte>>()))
+                .Callback<Span<byte>, Span<byte>>((name, value) =>
+                {
+                });
+            var obj = headersHandler.Object;
+        }
+    }
+
+    public interface IHttpHeadersHandler
+    {
+        void OnHeader(Span<byte> name, Span<byte> value);
     }
 }
